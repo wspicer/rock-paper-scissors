b3b462b2601a727582ba318e41b6f05956c9d102
Will Spicer <william.spicer766@gmail.com>
2022-12-20 20:43:55 +0000

    Added the prompt function to receive user input for playing against the computer.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
-const playerSelection = "rock";
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+let playerSelection = "";
+const computerSelection = getComputerChoice()
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -31,5 +32,19 @@ function playRound (playerSelection, computerSelection) {
         return "You Win";
     } else if (playerSelection === scissors && computerSelection === paper) {
         return "You Win";
+    }
+}
+
+
+function playerPrompt () {
+    let promptQuestion = prompt("Rock, paper, or Scissors? Crush, crinkle, snip if you will");
+    if (promptQuestion.toLowerCase() === "rock") {
+        playerSelection = "rock";
+    } else if (promptQuestion.toLowerCase() === "paper") {
+        playerSelection = "paper";
+    } else if (promptQuestion.toLowerCase() === "scissors") {
+        playerSelection = "scissors";
+    } else {
+        promptQuestion;
     }
 }
