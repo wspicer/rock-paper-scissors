4ee752d6b853d8e7f815135270a93683b7098237
Will Spicer <william.spicer766@gmail.com>
2022-12-21 17:39:49 +0000

    Added the seletion variable into the for loop so that they would reset each time the game is played. I changed the outcome of the if and else statments of the playRound function so that they instead would add 1 to the variables of playerWins and computerWins to keep track of the win and loss record.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-let playerSelection = playerPrompt();
-const computerSelection = getComputerChoice();
+let computerWins = 0;
+let playerWins = 0
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -10,7 +11,6 @@ function getComputerChoice() {
         return rock;
     } else if (choice === 2) {
         return paper;
-        
     } else {
         return scissors;
     }
@@ -21,17 +21,17 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "draw. Try again";
     } else if (playerSelection === rock && computerSelection === paper) {
-        return "Computer Wins this round";
+        computerWins += 1;
     } else if (playerSelection === paper && computerSelection === scissors) {
-        return "Computer Wins this round";
+        computerWins += 1;
     } else if (playerSelection === scissors && computerSelection === rock) {
-        return "Computer Wins this round";
+        computerWins += 1;
     } else if (playerSelection === rock && computerSelection === scissors) {
-        return "You Win this round";
+        playerWins += 1;
     } else if (playerSelection === paper && computerSelection === rock) {
-        return "You Win this round";
+        playerWins += 1;
     } else if (playerSelection === scissors && computerSelection === paper) {
-        return "You Win this round";
+        playerWins += 1;
     }
 }
 
@@ -43,8 +43,27 @@ function playerPrompt () {
     } else if (promptQuestion.toLowerCase() === "paper") {
         return paper;
     } else if (promptQuestion.toLowerCase() === "scissors") {
-        return "scissors";
+        return scissors;
     } else {
         promptQuestion;
     }
+}
+
+function game () {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = playerPrompt();
+        let computerSelection = getComputerChoice();
+        //console.log(playerPrompt());
+        //console.log(getComputerChoice());
+        console.log(playRound(playerSelection,computerSelection));
+        console.log(computerWins);
+        console.log(playerWins);
+     }
+     if (computerWins > playerWins) {
+        console.log("Computer Wins and had " + computerWins + " wins, and you only had " + playerWins + " wins");
+     } else if (computerWins < playerWins) {
+        console.log("You Win and had " + playerWins + " and the computer only had " + computerWins + " wins");
+     } else if (computerWins === playerWins) {
+        console.log("It's a draw!");
+     }
 }
